b51cf53512adbf1dfad91eeea055621d62454cfb
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-05-20 07:03:10 -0700

    Update gp-hidetag.js

--- plugins/gp-hidetag.js
+++ plugins/gp-hidetag.js
@@ -20,7 +20,7 @@ await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
 }
 handler.help = ['hidetag']
 handler.tags = ['group']
-handler.command = ['hidetag', 'notify'] 
+handler.command = ['htg', 'مخفي'] 
 handler.group = true
 handler.admin = true
 
