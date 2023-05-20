8053b52df9255c1954ef46626bd21d11497c01e6
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-05-20 06:50:57 -0700

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -5,11 +5,11 @@ let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 let av = `./src/mp3/${pickRandom(["Guru", "Guru1", "Guru2", "Guru3", "Guru4"])}.mp3`
 
-m.reply( `Hello ${taguser} Need help?  type /help `)
+m.reply( `مرحبا ${taguser} تريد المساعده؟  اكتب /اوامر `)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
-handler.customPrefix = /^(bot|guru)$/i
+handler.customPrefix = /^(بوت|صوفيا)$/i
 handler.command = new RegExp
 
 export default handler
