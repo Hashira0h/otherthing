3f444a58be84507b0d16b5246858f6cf9501be18
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-05-24 06:14:52 -0700

    Update gp-kick.js

--- plugins/gp-kick.js
+++ plugins/gp-kick.js
@@ -1,19 +1,19 @@
 
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
 	
-let kickte = `✳️ Correct use of the command\n*${usedPrefix + command}* @tag`
+let kickte = `✳️ الاستخدام الصحيح للامر\n*${usedPrefix + command}* @منشن`
 
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`✅ User kicked`) 
+m.reply(`تم طرده بنجاح`) 
 
 }
 
 handler.help = ['kick @user']
 handler.tags = ['group']
-handler.command = ['kick', 'expulsar'] 
+handler.command = ['kick', 'طرد'] 
 handler.admin = true
 handler.group = true
 handler.botAdmin = true
