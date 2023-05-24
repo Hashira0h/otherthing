418d88842c3cb3ab343a4848b2d2d0aa1ca78a94
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-05-24 06:52:00 -0700

    Update gp-warning.js

--- plugins/gp-warning.js
+++ plugins/gp-warning.js
@@ -4,36 +4,36 @@ let handler = async (m, { conn, text, args, groupMetadata, usedPrefix, command }
         let who
         if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
         else who = m.chat
-        if (!who) throw `✳️ Tag or mention someone\n\n📌 Example : ${usedPrefix + command} @user`
-        if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+        if (!who) throw `❒ منشن شخص ما\n\n❍ مثال : ${usedPrefix + command} @الشخص`
+        if (!(who in global.db.data.users)) throw `❒ المستخدم غير موجود في قاعدة البيانات الخاصة بي`
         let name = conn.getName(m.sender)
         let warn = global.db.data.users[who].warn
         if (warn < war) {
             global.db.data.users[who].warn += 1
             m.reply(`
-⚠️ *Warned User* ⚠️
+⚠️ *تحذير المستخدم* ⚠️
 
-▢ *Admin:* ${name}
-▢ *Usuario:* @${who.split`@`[0]}
-▢ *Warns:* ${warn + 1}/${war}
-▢ *Reason:* ${text}`, null, { mentions: [who] }) 
+❒ *المشرف:* ${name}
+❒ *مستخدم:* @${who.split`@`[0]}
+❒ *الانذارات:* ${warn + 1}/${war}
+❒ *السبب:* ${text}`, null, { mentions: [who] }) 
             m.reply(`
-⚠️ *caution* ⚠️
-You received a warning from an admin
+⚠️ *تحذير* ⚠️
+لقد تلقيت تحذيرًا من المشرف
 
-▢ *Warns:* ${warn + 1}/${war} 
-if you receive *${war}* warnings you will be automatically removed from the group`, who)
+❒ *الانذارات:* ${warn + 1}/${war} 
+اذا تلقيت *${war}* تحذيرات سيتم إزالتك تلقائيًا من المجموعة`, who)
         } else if (warn == war) {
             global.db.data.users[who].warn = 0
-            m.reply(`⛔ The user exceeded the *${war}* warnings will therefore be removed`)
+            m.reply(`❍ تجاوز المستخدم ال *${war}* من التحذيرات لذلك سيتم إزالة الشخص`)
             await time(3000)
             await conn.groupParticipantsUpdate(m.chat, [who], 'remove')
-            m.reply(`♻️ You were removed from the group *${groupMetadata.subject}* because you have been warned *${war}* times`, who)
+            m.reply(`❍ لقد تم ازالتك من الجروب *${groupMetadata.subject}* لان تم تحذيرك *${war}* مره`, who)
         }
 }
 handler.help = ['warn @user']
 handler.tags = ['group']
-handler.command = ['warn'] 
+handler.command = ['انذار'] 
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
