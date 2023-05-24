bd1e8b40268b201eca6e45ae829cdf9814a65516
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-05-24 06:05:34 -0700

    Update gp-delwarn.js

--- plugins/gp-delwarn.js
+++ plugins/gp-delwarn.js
@@ -4,23 +4,23 @@ let handler = async (m, { conn, args, groupMetadata}) => {
         if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
         else who = m.chat
         if (!who) throw `✳️ Tag or mention someone`
-        if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+        if (!(who in global.db.data.users)) throw `✳️ المستخدم غير موجود في قاعدة البيانات الخاصة بي`
        let warn = global.db.data.users[who].warn
        if (warn > 0) {
          global.db.data.users[who].warn -= 1
-         m.reply(`⚠️ *DELWARN*
+         m.reply(`⚠️ *حذف الانذار*
          
-▢ Warns: *-1*
-▢ Warns total: *${warn - 1}*`)
-         m.reply(`✳️ An admin lowered their warning, now you have *${warn - 1}*`, who)
+▢الانذار : *-1*
+▢ عدد الانذارات الحالي: *${warn - 1}*`)
+         m.reply(`✳️ قام أحد المسؤولين بتخفيض تحذيره ، والآن لديك *${warn - 1}*`, who)
          } else if (warn == 0) {
-            m.reply('✳️ The user has no warning')
+            m.reply('✳️ المستخدم ليس لديه انذارات')
         }
 
 }
 handler.help = ['delwarn @user']
 handler.tags = ['group']
-handler.command = ['delwarn', 'unwarn'] 
+handler.command = ['حذف_انذار', 'حذف_تحذير'] 
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
