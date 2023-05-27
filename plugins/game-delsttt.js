e53d5f8d055c9d26ca2ac5e1e81752ee9eea2564
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-05-27 14:44:59 -0700

    Update game-delsttt.js

--- plugins/game-delsttt.js
+++ plugins/game-delsttt.js
@@ -1,12 +1,12 @@
 
 let handler = async (m, { conn, text }) => {
 	let room = Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))
-if (room == undefined) return conn.reply(m.chat,`✳️ You are not in game of TicTacToe 🎮 `, m)
+if (room == undefined) return conn.reply(m.chat,`❏ انت لست في جيم !`, m)
 delete conn.game[room.id]
-await conn.reply(m.chat, `✅ The session of *tictactoe is restarted 🎮*`, m)
+await conn.reply(m.chat, `❏ تم حذف الجيم بنجاح`, m)
 }
 handler.help = ['delttt']
 handler.tags = ['game']
-handler.command = ['delttc', 'delttt', 'delxo']
+handler.command = ['حذففف', 'delttt', 'delxo']
 
 export default handler
