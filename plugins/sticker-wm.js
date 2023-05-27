8fbf657c4ed8e7afdf4827e1bf4168748220b76a
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-05-27 13:45:13 -0700

    Update sticker-wm.js

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -1,16 +1,16 @@
 
 import { addExif } from '../lib/sticker.js'
 let handler = async (m, { conn, text, args }) => {
-  if (!m.quoted) throw 'respond to a sticker'
+  if (!m.quoted) throw '❉ رد على ملصق'
   let stiker = false
        let stick = args.join(" ").split("|");
        let f = stick[0] !== "" ? stick[0] : packname;
        let g = typeof stick[1] !== "undefined" ? stick[1] : author;
   try {
     let mime = m.quoted.mimetype || ''
-    if (!/webp/.test(mime)) throw 'respond to a sticker'
+    if (!/webp/.test(mime)) throw '❉ رد على ملصق'
     let img = await m.quoted.download()
-    if (!img) throw 'Responde to sticker!'
+    if (!img) throw '❉ رد على ملصق !!!'
     stiker = await addExif(img, f, g)
   } catch (e) {
     console.error(e)
@@ -22,6 +22,6 @@ let handler = async (m, { conn, text, args }) => {
 }
 handler.help = ['take <name>|<author>']
 handler.tags = ['sticker']
-handler.command = ['take', 'wm'] 
+handler.command = ['سرقة', 'wm', 'سرقه'] 
 
 export default handler
