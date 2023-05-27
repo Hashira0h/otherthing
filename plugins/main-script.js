d0aa6e08e8747ace2ed2c59857ad512e76ab7287
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-05-27 14:12:59 -0700

    Update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -5,15 +5,15 @@ let handler = async function (m, { conn, __dirname }) {
 let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
   
 m.reply(`
-*≡ SCRIPT*
+*الاسكربت*
 
-▢ Git : ${_package.homepage}
+❒ تفضل : مغفل
 `.trim())
     
 }
 
 handler.help = ['script']
 handler.tags = ['main']
-handler.command = ['sc', 'git', 'script'] 
+handler.command = ['sc', 'git', 'سكربت'] 
 
 export default handler
