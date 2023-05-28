8a94e828a0080287b0250fb443804506c1d6b561
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-05-28 06:19:33 -0700

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -9,7 +9,6 @@ export async function before(m, { conn }) {
     while (canLevelUp(user.level, user.exp, global.multiplier))
         user.level++
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
     user.role = global.rpg.role(user.level).name
     if (before !== user.level) {
         m.reply(`
