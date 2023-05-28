8b42133eb89967db530892754757a609d8ea6fd0
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-05-28 09:18:17 -0700

    Update gp-staff.js

--- plugins/gp-staff.js
+++ plugins/gp-staff.js
@@ -1,14 +1,14 @@
 let handler = async (m, { conn, participants, groupMetadata, args }) => {
 const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/avatar_contact.png'
 const groupAdmins = participants.filter(p => p.admin)
-const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n▢ ')
+const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n♕ ')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 
 let text = `
 ❒ *مشرفين المجموعه ♡* _${groupMetadata.subject}_
 
 ─── *المشرفين* ───
-❒ه  ${listAdmin}
+♕  ${listAdmin}
 ───────────ه┘
 `.trim()
 conn.sendFile(m.chat, pp, 'staff.png', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })
