aac231e36aba70e32824026c8a80402677658c16
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-05-28 08:13:44 -0700

    Update anime-rpat.js

--- plugins/anime-rpat.js
+++ plugins/anime-rpat.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
    let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
-     if (!who) throw `✳️ Tag or mention someone\n\n📌 Example : ${usedPrefix + command} @tag` 
+     if (!who) throw `✽ منشن الشخص اللي تربيتك\n\n📌 مثال : ${usedPrefix + command} @منشن` 
      
     let user = global.db.data.users[who]
     let name = conn.getName(who) 
@@ -26,7 +26,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 
 handler.help = ['pat @tag']
 handler.tags = ['rnime']
-handler.command = /^(acariciar|pat)$/i
+handler.command = /^(تربيتي|pat)$/i
 handler.diamond = true
 handler.group = true
 
