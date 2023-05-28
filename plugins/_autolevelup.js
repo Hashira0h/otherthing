7e9bdd17222381fc8c99c132ac934c7c674ef357
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-05-28 05:43:45 -0700

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -9,11 +9,13 @@ export async function before(m, { conn }) {
     while (canLevelUp(user.level, user.exp, global.multiplier))
         user.level++
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn } = global.db.data.users[who]
     user.role = global.rpg.role(user.level).name
     if (before !== user.level) {
         m.reply(`
 *❏ لـفـل جـديـد*
-الاسم : ${taguser}
+الاسم : ${name} 
+المنشن : ${taguser}
 الـقديم : *${before}* \nالـجـديـد : *${user.level}*
  تـصـنـيـفـك : *${user.role}*
  
