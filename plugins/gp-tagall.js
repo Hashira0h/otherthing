cfed19d31c17f38972628da646ed8f364a3314f1
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-05-28 09:15:24 -0700

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,6 +1,7 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`⧉ جروب : *${groupMetadata.subject}*\n⧉ عدد الاعضاء : *${participants.length}*${text ? `\n⧉ الرساله : ${text}\n` : ''}\n───⊷ *المنشن الجماعي* ⊷───\n` + users.map(v => 'ه♡⇠ @' + v.replace(/@.+/, '')).join`\n` + '\n└──☆ sᴏғɪᴀ ᴮᴼᵀ ☆──', null, {
+const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n♕ ')
+    m.reply(`⧉ جروب : *${groupMetadata.subject}*\n⧉ عدد الاعضاء : *${participants.length}*${text ? `\n⧉ الرساله : ${text}\n` : ''}\nالمشرفين : ${listAdmin}\n───⊷ *المنشن الجماعي* ⊷───\n` + users.map(v => 'ه♡⇠ @' + v.replace(/@.+/, '')).join`\n` + '\n└──☆ sᴏғɪᴀ ᴮᴼᵀ ☆──', null, {
         mentions: users
     })
 }
