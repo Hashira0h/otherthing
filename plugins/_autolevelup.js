48d84fa46374cf2d5a9885a7f803d8f3f3fa1d1f
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-05-30 14:01:47 -0700

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -4,21 +4,19 @@ import { canLevelUp } from '../lib/levelling.js'
 export async function before(m, { conn }) {
     let user = global.db.data.users[m.sender]
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn } = global.db.data.users[who]
-let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
+let username = conn.getName(who)
     if (!user.autolevelup)
         return !0
     let before = user.level * 1
     while (canLevelUp(user.level, user.exp, global.multiplier))
         user.level++
-let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
     user.role = global.rpg.role(user.level).name
     if (before !== user.level) {
         m.reply(`
 *❏ لـفـل جـديـد*
 
-الاسم : ${name}
-المنشن : ${registered ? '\n   • ' + name + ' ': ''}
+الاسم : ${username}
+المنشن : @${who.replace(/@.+/, '')}
 
 الـقديم : *${before}* \n\nالـجـديـد : *${user.level}*
 
