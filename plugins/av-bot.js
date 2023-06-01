a7778b6b48bd88ad67c595b3c56351abe26fb175
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-01 03:43:03 -0700

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn}) => {
 let user = global.db.data.users[m.sender]
 let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let av = `./src/mp3/${pickRandom(["Guru", "Guru1", "Guru2", "Guru3", "Guru4"])}.mp3`
+let av = `./src/mp3/${pickRandom(["levi"])}.mp3`
 
 m.reply( `مرحبا ${taguser}\nاكتب قبل كل امر [.]
 
