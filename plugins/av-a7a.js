9baab4614fcdd1350fd4391ea6c08fe87ee8eea4
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-01 06:34:43 -0700

    Update av-a7a.js

--- plugins/av-a7a.js
+++ plugins/av-a7a.js
@@ -1,11 +1,17 @@
 let handler = async (m, { conn}) => {
 let user = global.db.data.users[m.sender]
-let name = conn.getName(m.sender)
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+let username = conn.getName(who)
+let bv = `./src/mp3/${pickRandom([""])}.jpg`
+let av = `./src/mp3/${pickRandom([""])}.mp3`
 
-m.reply( `*الفاظك يعم الحج*`
+m.reply( `*الفاظك يعم الحج*`)
+conn.sendFile(m.chat, bv, 'picturejpg', null, m, true, { type: 'pictureMessage', ptt: true })
+conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+}
 
-handler.customPrefix = /^(احاا|احا)$/i
+handler.customPrefix = /^(احا|احاا)$/i
 handler.command = new RegExp
 
 export default handler
