5e1e2c02d604b5e6ef28803e6c8b160265896c80
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-01 06:21:35 -0700

    Update Wall-Messi.js

--- plugins/Wall-Messi.js
+++ plugins/Wall-Messi.js
@@ -3,8 +3,8 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 let res = (await axios.get(`https://raw.githubusercontent.com/Guru322/api/Guru/BOT-JSON/Messi.json`)).data  
 let url = await res[Math.floor(res.length * Math.random())]
 conn.sendFile(m.chat, url, 'error.jpg', `*Messi*`, m)} 
-//conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
+//conn.sendButton(m.chat, "*ميسي*", author, url, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
 handler.help = ['messi']
 handler.tags = ['internet']
-handler.command = /^(messi)$/i
+handler.command = /^(ميسي)$/i
 export default handler
