6fba99d0abcdbddc6e5a7208759764c80db7163d
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-01 04:52:27 -0700

    Update main-Aizen.js

--- plugins/main-Aizen.js
+++ plugins/main-Aizen.js
@@ -1,1 +1,18 @@
+let handler = async (m, { conn}) => {
+let user = global.db.data.users[m.sender]
+let name = conn.getName(m.sender)
+let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+let av = `./src/mp3/${pickRandom(["aizen"])}.mp3`
 
+m.reply( `ايزن هو عمي`)
+conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+} 
+
+handler.customPrefix = /^(عمك|صوفيا مين عمك)$/i
+handler.command = new RegExp
+
+export default handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
