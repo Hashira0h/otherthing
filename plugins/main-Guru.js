98f3181dca8ab310fa5fd0383038d1b32aaab967
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-01 12:16:08 -0700

    Update main-Guru.js

--- plugins/main-Guru.js
+++ plugins/main-Guru.js
@@ -1,29 +1,29 @@
 let handler = async m => m.reply(`
 
-≡  *GURU  ┃ ᴮᴼᵀ*   GROUPS
-
-─────────────
-▢ Join public bot group and support
-https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp
-
-▢ Group 2
-https://chat.whatsapp.com/LwLQab5mZVW6WWRbBPnDGC
-
-▢ Group 3
-https://chat.whatsapp.com/Jn9qVerYN6Q1L0vlVFe2XL
-
-─────────────
-≡ Disabled links? enter here! 
-
-▢ Group WhatsApp 
- https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp
-─────────────
-▢ *Owner Telegram*
- https://t.me/i_want_to_be_isekaied
-
-▢ *YouTube*
-• https://www.youtube.com/@Asliguru
-
+┐ جروبات *MiKASA  ┃ ᴮᴼᵀ* ┌ 
+│
+│ ‐ ‑ ‒ – — ―
+│ ◊ ادخل جروب البوت
+│
+│
+│ ◊ جروب 2
+│
+│
+│ ◊ جروب 3
+│
+│
+│ ‐ ‑ ‒ – — ―
+│ ❍ الروابط ليست صحيحه؟ ادخل هنا 
+│
+│ ◊ جروب واتس اب 
+│ 
+│ ‐ ‑ ‒ – — ―
+│ ◊ *تيليجرام المطور*
+│
+│
+│ ◊ *قناة اليوتيوب*
+│ ● 
+└ه *MiKASA  ┃ ᴮᴼᵀ*
 
 `.trim())
 handler.help = ['gpguru']
