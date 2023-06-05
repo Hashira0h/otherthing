b2ad6f59f51893be011581ee8ee9434de4f81a5a
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-05 08:51:12 -0700

    Update fun-simsimi.js

--- plugins/fun-simsimi.js
+++ plugins/fun-simsimi.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 	
  let name = conn.getName(m.sender)
-  if (!text) throw `اهلا *${name}* تريد ان تتكلم معي؟ \nاكتب *${usedPrefix + command}* (رسالتك) \n\n📌 مثال : *${usedPrefix + command}* مرحبا يا بوت`
+  if (!text) throw `مرحبا *${name}* اريد ان تتحدث معي ؟ \nاكتب *${usedPrefix + command}* (رسالتك) \n\n📌 مثال : *${usedPrefix + command}* مرحبا يا بوت`
   m.react('🗣️') 
   //let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(m.text), lc: "es" }, ''))
   let res = await fetch(`https://api.simsimi.net/v2/?text=${text}&lc=en`)
@@ -13,6 +13,6 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 }
 handler.help = ['bot']
 handler.tags = ['fun']
-handler.command = ['bot', 'صوفيا'] 
+handler.command = ['bot', 'ميكاسا'] 
 
 export default handler
