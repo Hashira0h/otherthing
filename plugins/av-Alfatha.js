b47f51e052267680c9ee32fb7ee48b56cc3fe704
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-04 20:11:12 -0700

    Create av-Alfatha.js

---
+++ plugins/av-Alfatha.js
@@ -0,0 +1,21 @@
+let handler = async (m, { conn}) => {
+let user = global.db.data.users[m.sender]
+let name = conn.getName(m.sender)
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+let username = conn.getName(who)
+let av = `./src/mp3/${pickRandom(["alfatha"])}.mp3`
+
+m.reply( `(بِسۡمِ ٱللَّهِ ٱلرَّحۡمَٰنِ ٱلرَّحِيمِ (١
+ٱلۡحَمۡدُ لِلَّهِ رَبِّ ٱلۡعَٰلَمِينَ (٢) ٱلرَّحۡمَٰنِ ٱلرَّحِيمِ (٣) مَٰلِكِ يَوۡمِ ٱلدِّينِ (٤) إِيَّاكَ نَعۡبُدُ وَإِيَّاكَ نَسۡتَعِينُ (٥) ٱهۡدِنَا ٱلصِّرَٰطَ ٱلۡمُسۡتَقِيمَ (٦) صِرَٰطَ ٱلَّذِينَ أَنۡعَمۡتَ عَلَيۡهِمۡ غَيۡرِ ٱلۡمَغۡضُوبِ عَلَيۡهِمۡ وَلَا ٱلضَّآلِّينَ ٧` 
+conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+} 
+
+handler.customPrefix = /^(الفاتحه|الفاتحة)$/i
+handler.command = new RegExp
+
+export default handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
