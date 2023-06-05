e5076bfe8503a0310c0b0cd2a3f185f1f027d461
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-04 19:32:30 -0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -14,10 +14,10 @@ export async function before(m, {conn, isAdmin, isBotAdmin }) {
             const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
             if (m.text.includes(linkThisGroup)) return !0
         }
-        await conn.reply(m.chat, `*≡ Link Detected*
+        await conn.reply(m.chat, `*تم اكتشاف الرابط*
             
-We do not allow links from other groups 
-I'm sorry *@${m.sender.split('@')[0]}*  you will be kicked out of the group ${isBotAdmin ? '' : '\n\nIM not an admin so I canT expel you :"v'}`, null, { mentions: [m.sender] } )
+نحن لا نسمح بذلك 
+تبا لك يا *@${m.sender.split('@')[0]}*  سوف اطردك ${isBotAdmin ? '' : '\n\nانا لست مشرف لاتخاذ اي اجراء :"v'}`, null, { mentions: [m.sender] } )
         if (isBotAdmin && chat.antiLink) {
         	await conn.sendMessage(m.chat, { delete: m.key })
             await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
