ca6c68556194d4ec1fc65f3cacab45c21816da92
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-06 13:59:36 -0700

    Update main-Guru.js

--- plugins/main-Guru.js
+++ plugins/main-Guru.js
@@ -2,32 +2,24 @@ let handler = async m => m.reply(`
 
 ┐ جروبات *MiKASA  ┃ ᴮᴼᵀ* ┌ 
 │ه
-│ه ‐ ‑ ‒ – — ―‐ ‑ ‒ – — ―
-│ ◊ ادخل جروب البوت
+│ه ‐ ‑ ‒ – — ―
+│◊ ادخل جروب البوت
 │ه
-│ه
-│ ◊ جروب 2
-│ه
-│ه
-│ ◊ جروب 3
-│ه
-│ه
-│ه ‐ ‑ ‒ – — ―‐ ‑ ‒ – — ―
-│ ❍ الروابط ليست صحيحه؟ ادخل هنا 
-│ه
-│ ◊ جروب واتس اب 
+│ه *https://chat.whatsapp.com/BYcNmfA9dbzDddTFrsAdoc*
+│◊ نقابة
 │ه 
-│ه ‐ ‑ ‒ – — ―‐ ‑ ‒ – — ―
-│ ◊ *تيليجرام المطور*
+│ه *https://chat.whatsapp.com/Dl9QZ0xn6JlIkd9glPmoZH* 
+│◊ جروب خلفيات
 │ه
-│ه
-│ ◊ *قناة اليوتيوب*
-│ه ● 
+│ه *https://chat.whatsapp.com/HYkO0WWGlpO9AeaaiNeimf*
+│ه ‐ ‑ ‒ – — ―
+│◊ *قناة اليوتيوب*
+│ه ● *https://www.youtube.com/@Mo_yt74*
 ┘ه *MiKASA  ┃ ᴮᴼᵀ* └
 
 `.trim())
 handler.help = ['gpguru']
 handler.tags = ['main']
-handler.command = ['groups', 'groupguru', 'gugp', 'ggp', 'gpguru'] 
+handler.command = ['groups', 'mikasagp', 'mkgp', 'ggp', 'الجروبات'] 
 
 export default handler
