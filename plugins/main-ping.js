17522d7e1aef1d029b93336524f7a5f3602fa2bc
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-06 14:42:43 -0700

    Update main-ping.js

--- plugins/main-ping.js
+++ plugins/main-ping.js
@@ -8,11 +8,11 @@ let handler = async (m, { conn }) => {
          exec(`neofetch --stdout`, (error, stdout, stderr) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
-          m.reply(`${ssd}🟢 *pong* : ${latensi.toFixed(4)} _ms_`);
+          m.reply(`${ssd}🟢 *بونج* : ${latensi.toFixed(4)} _ms_`);
             });
 }
 handler.help = ['ping']
 handler.tags = ['main']
-handler.command = ['ping', 'speed']
+handler.command = ['بينج', 'السرعة']
 
 export default handler
