59994644d281d8988a83d943760d8ce024fd2cfa
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-06 14:04:42 -0700

    Update main-blocklist.js

--- plugins/main-blocklist.js
+++ plugins/main-blocklist.js
@@ -4,18 +4,18 @@ let handler = async (m, { conn }) => {
 	await conn.fetchBlocklist().then(async data => {
 		let txt = `*≡ List *\n\n*Total :* ${data.length}\n\n┌─⊷\n`
 		for (let i of data) {
-			txt += `▢ @${i.split("@")[0]}\n`
+			txt += `➹ @${i.split("@")[0]}\n`
 		}
-		txt += "└───────────"
+		txt += "╝════〖 ه 〗════╚"
 		return conn.reply(m.chat, txt, m, { mentions: await conn.parseMention(txt) })
 	}).catch(err => {
 		console.log(err);
-		throw 'no numbers blocked'
+		throw 'مفيش ارقام متبلكه'
 	})
 }
 
 handler.help = ['blocklist']
 handler.tags = ['main']
-handler.command = ['blocklist', 'listblock'] 
+handler.command = ['blocklist', 'المتبلكين'] 
 
 export default handler
