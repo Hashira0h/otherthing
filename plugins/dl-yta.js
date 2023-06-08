c792bd3718179b3c24757f9322ff1c00e1cc8a1e
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-07 21:40:36 -0700

    Update dl-yta.js

--- plugins/dl-yta.js
+++ plugins/dl-yta.js
@@ -1,8 +1,8 @@
 import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args }) => {
-if (!args[0]) throw '*[❗𝐈𝐍𝐅𝐎❗] SEARCH YOUTUBE / YOUTUBE 𝙻𝙸𝙽𝙺*'
-await m.reply(`*_⏳processing ᴀᴜᴅɪᴏ...⏳_*\n\n*◉  command #playdoc ᴏ #play.2 ᴏ #ytmp4doc ◉*`)
+if (!args[0]) throw '*هات رابط من يوتيوب*'
+await m.reply(`*_⏳جاري تجهيز الملف...⏳_*`)
 try {
 let q = '128kbps'
 let v = args[0]
@@ -18,7 +18,7 @@ let lolh = await lolhuman.json()
 let n = lolh.result.title || 'error'
 await conn.sendMessage(m.chat, { audio: { url: lolh.result.link }, fileName: `${n}.mp3`, mimetype: 'audio/mp4' }, { quoted: m })   
 } catch {
-await conn.reply(m.chat, '*[❗]AUDIO ERROR*', m)}
+await conn.reply(m.chat, '*خطأ*', m)}
 }}
-handler.command = /^fgmp3|dlmp3|getaud|yt(a|mp3)$/i
+handler.command = /^fgmp3|اغنية|getaud|yt(a|mp3)$/i
 export default handler
