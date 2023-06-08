409f9fa999ca21d272e8fb824aefd4e670c6235b
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-07 19:46:36 -0700

    Update GURU-Openai.js

--- plugins/GURU-Openai.js
+++ plugins/GURU-Openai.js
@@ -1,14 +1,14 @@
 import fetch from 'node-fetch'
 let handler = async (m, { text, usedPrefix, command }) => {
-if (!text) throw `*enter a request or an order to use the chatgpt*\n\n*—◉ 𝙴xample*\n*◉ ${usedPrefix + command}series 2022  netflix*\n*◉ ${usedPrefix + command} write a js code*`
+if (!text) throw `*ادخل الشيء الذي تريد الذكاء الاصطناعي الرد عليه*\n\n*مثال*\n*◉ ${usedPrefix + command}من اخذ كأس العالم عام 2000*\n*◉ ${usedPrefix + command} اكتب كود من لغة جافاسربت*`
 try {
-m.reply(`*wait sometime*`)
+m.reply(`*انتظر...*`)
 let tiores = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=${text}&user=user-unique-id`)
 let hasil = await tiores.json()
 m.reply(`${hasil.result}`.trim())
 } catch {
 throw `*𝙴𝚁𝚁𝙾𝚁*`
 }}
-handler.command = ['bro', 'chatgpt', 'ai', 'siri']
+handler.command = ['bro', 'chatgpt', 'ai', 'siri', 'روبوت']
 handler.diamond = false
 export default handler
