5eac8dcd7d1747c6a040f6001f598ee040bdde5c
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-07 21:10:59 -0700

    Update dl-tiktokstalk.js

--- plugins/dl-tiktokstalk.js
+++ plugins/dl-tiktokstalk.js
@@ -5,19 +5,19 @@ let handler = async (m, { conn, text, args }) => {
   if (!text) throw `✳️Enter the Username of a TikTok user `
   let res = await fg.ttStalk(args[0])
   let txt = `
-┌──「 *TIKTOK STALK* 
-▢ *🔖Number:* ${res.name}
-▢ *🔖Username:* ${res.username}
-▢ *👥followers:* ${res.followers}
-▢ *🫂following:* ${res.following}
-▢ *📌Desc:* ${res.desc}
+┐────【 *تيك توك* 】
+□ *🔖الاسم:* ${res.name}
+□ *🔖اسم المستخدم:* ${res.username}
+□ *👥عدد المتابعين:* ${res.followers}
+□ *🫂يتابع:* ${res.following}
+□ *📌الوصف:* ${res.desc}
 
-▢ *🔗 Link* : https://tiktok.com/${res.username}
-└────────────`
+□ *🔗 الرابط* : https://tiktok.com/${res.username}
+└──────────────`
   await conn.sendFile(m.chat, res.profile, 'tt.png', txt, m)
 }
 handler.help = ['tiktokstalk']
 handler.tags = ['dl']
-handler.command = /^t(tstalk|iktokstalk)$/i
+handler.command = /^t(tstalk|تيكتوك2)$/i
 
 export default handler
