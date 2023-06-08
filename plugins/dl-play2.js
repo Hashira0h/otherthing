f99de9ed8201485b6efb3c74d0ab9f4ca8b9597e
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-07 20:41:03 -0700

    Update dl-play2.js

--- plugins/dl-play2.js
+++ plugins/dl-play2.js
@@ -3,21 +3,21 @@ import { youtubeSearch } from '@bochilteam/scraper'
 import yts from 'yt-search'
 let handler = async(m, { conn, usedPrefix, text, args, command }) => {
 
-    if (!text) throw `✳️ Enter a song title\n\n*📌 Example*\n*${usedPrefix + command}* Lil Peep hate my fuccn life `
+    if (!text) throw `ادخل اسم الشيء الذي تريد البحث عنه !\n\n*📌 مثال :*\n*${usedPrefix + command}* ايتوشي رين`
     m.react('📀')
     let result = await yts(text)
     let ytres = result.all
     let listSections = []
 	Object.values(ytres).map((v, index) => {
 	listSections.push([`${index}┃ ${v.title}`, [
-          ['🎶 MP3', `${usedPrefix}fgmp3 ${v.url}`, `▢ ⌚ *Duration:* ${v.timestamp}\n▢ 👀 *Views:* ${v.views}\n▢ 📌 *Títle* : ${v.title}\n▢ 📆 *Publiced:* ${v.ago}\n`],
-          ['🎥 MP4', `${usedPrefix}fgmp4 ${v.url}`, `▢ ⌚ *Duratión:* ${v.timestamp}\n▢ 👀 *Views:* ${v.views}\n▢ 📌 *Títle* : ${v.title}\n▢ 📆 *Publiced:* ${v.ago}\n`]
+          ['🎶 MP3', `${usedPrefix}fgmp3 ${v.url}`, `■ *المده:* ${v.timestamp}\n■ 👀 *المشاهدات:* ${v.views}\n■ 📌 *العنوان* : ${v.title}\n■ 📆 *منشورة:* ${v.ago}\n`],
+          ['🎥 MP4', `${usedPrefix}fgmp4 ${v.url}`, `■ *المده:* ${v.timestamp}\n■ 👀 *المشاهدات:* ${v.views}\n■ 📌 *العنوان* : ${v.title}\n■ 📆 *منشورة:* ${v.ago}\n`]
         ]])
 	})
-	return conn.sendList(m.chat, '  ≡ *GURU MUSIC*🔎', `\n 📀 Here a list of results from :\n *${text}*`, igfg, `Click `, listSections, m)
+	return conn.sendList(m.chat, '  ◌ *MIKASA MUSIC* ◌', `\n 📀 هنا قائمة النتائج من :\n *${text}*`, igfg, `Click `, listSections, m)
 }
 handler.help = ['play2']
 handler.tags = ['dl']
-handler.command = ['play2', 'playvid2', 'playlist', 'playlista'] 
+handler.command = ['شغل2', 'playvid2', 'playlist', 'playlista'] 
 
 export default handler
