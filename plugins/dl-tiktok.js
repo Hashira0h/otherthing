854b5c59da9cc36def6d3b1969c2743178cb172f
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-07 21:02:43 -0700

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -3,17 +3,17 @@ import fg from 'api-dylux'
 import { tiktokdl, tiktokdlv2, tiktokdlv3 } from '@bochilteam/scraper'
 
 let handler = async (m, { conn, text, args, usedPrefix, command}) => {
-if (!args[0]) throw `✳️ Enter a Tiktok link\n\n 📌 Example : ${usedPrefix + command} https://vm.tiktok.com/ZMNqyusVD/?k=1`
-if (!args[0].match(/tiktok/gi)) throw `❎ verify that the link is from tiktok`
+if (!args[0]) throw `☆ ادخل رابط تيك توك\n\n 📌 مثال : ${usedPrefix + command} https://www.tiktok.com/@svsvkes/video/7177431175273794822?is_from_webapp=1&sender_device=pc&web_id=7242157033230681606`
+if (!args[0].match(/tiktok/gi)) throw `❎ تأكد ان هذا الرابط من تيك توك`
 m.react(rwait)
 
 try {
     let p = await fg.tiktok(args[0]) 
     let te = `
-┌─⊷ TIKTOK
-▢ *Username:* ${p.author}
-▢ *Descripción:* ${p.title}
-└───────────`
+■━━【 تيك توك 】━━■
+□ *اسم المستخدم:* ${p.author}
+□ *الوصف:* ${p.title}
+└───────────────`
     conn.sendButton(m.chat, te, igfg, p.nowm, [['⎘ Stalkig', `${usedPrefix}ttstalk ${p.author.replace(/^@/, '')}`], ['♫ Audio', `${usedPrefix}tomp3`]], m)
     m.react(done)
     } catch {  	
@@ -21,18 +21,18 @@ try {
          .catch(async _ => await tiktokdlv2(args[0]))
          .catch(async _ => await tiktokdlv3(args[0]))
     const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
-    if (!url) throw '❎ Error downloading the video'
+    if (!url) throw '❎ حدث خطأ ما اثناء تنزيل الفيديو'
     conn.sendButton(m.chat, `
-┌─⊷ TIKTOK
-▢ *Nickname:* ${nickname} ${description ? `\n▢ *Description:* ${description}` : ''}
-└───────────`, igfg, url, [['♫ Audio', `${usedPrefix}tomp3`]], m)
+■━━【 تيك توك 】━━■
+□ *الاسم:* ${nickname} ${description ? `\n▢ *Description:* ${description}` : ''}
+└───────────────`, igfg, url, [['♫ Audio', `${usedPrefix}tomp3`]], m)
 m.react(done)
 } 
     
 }  
 handler.help = ['tiktok']
 handler.tags = ['dl']
-handler.command = /^(tiktok|ttdl|tiktokdl|tiktoknowm)$/i
+handler.command = /^(tiktok|ttdl|tiktokdl|تيكتوك)$/i
 handler.diamond = true
 
 export default handler
