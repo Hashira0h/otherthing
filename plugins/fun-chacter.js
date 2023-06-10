cf007da67ff5a641e494fc38a058e1b5f326addf
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-09 18:40:42 -0700

    Update fun-chacter.js

--- plugins/fun-chacter.js
+++ plugins/fun-chacter.js
@@ -1,35 +1,35 @@
 let handler = async (m, { conn, command, text, usedPrefix, participants }) => {
-    if (!text) throw "Mention whose you want to check character"
+    if (!text) throw "منشن الشخص الذي تريد معرفة شخصيته"
     const mentionedUser = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : args[2] ? (args[2].replace(/[@ .+-]/g, '') + '@s.whatsapp.net') : ''
  const userChar = [
-      "Sigma",
-      "Generous",
-      "Grumpy",
-      "Overconfident",
-      "Obedient",
-      "Good",
-      "Simp",
-      "Kind",
-      "Patient",
-      "Pervert",
-      "Cool",
-      "Helpful",
-      "Brilliant",
-      "Sexy",
-      "Hot",
-      "Gorgeous",
-      "Cute",
+      "سيجما",
+      "كريم",
+      "غبي",
+      "صبور",
+      "خاين",
+      "حيوان",
+      "سيمب",
+      "لطيف",
+      "قليل الادب",
+      "نسونجي",
+      "محترم",
+      "مفيش منه اتنين",
+      "ذكي",
+      "خجول",
+      "تافه",
+      "مز/ه",
+      "كيوت",
     ]
     const userCharacterSeletion =
       userChar[Math.floor(Math.random() * userChar.length)]
 
-    let message = `Character of @${mentionedUser.split("@")[0]}  is *${userCharacterSeletion}* 🔥⚡`
+    let message = `شخصية @${mentionedUser.split("@")[0]}  هي *${userCharacterSeletion}* 🔥⚡`
     
     conn.sendMessage(m.chat, { text: message, mentions: [mentionedUser] }, { quoted: m })
     
 }
 handler.help = ["character @tag"]
 handler.tags = ['fun']
-handler.command = /^(character)/i
+handler.command = /^(شخصية)/i
 
 export default handler 
