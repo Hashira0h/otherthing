c202967b27b9989ca5c945d2d4bd4f8a313239ae
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-10 08:47:03 -0700

    Update main-Aizen.js

--- plugins/main-Aizen.js
+++ plugins/main-Aizen.js
@@ -3,15 +3,15 @@ let user = global.db.data.users[m.sender]
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 let username = conn.getName(who)
-let bv = `./src/mp3/${pickRandom(["Aizen"])}.jpg`
-let av = `./src/mp3/${pickRandom(["aizen"])}.mp3`
+let bv = `./src/mp3/${pickRandom([""])}.jpg`
+let av = `./src/mp3/${pickRandom([""])}.mp3`
 
-m.reply( `يا ${username} ايزن هو عمي 🫡🤍`)
+m.reply( `أتاك يقفل ملفه + انقلع`)
 conn.sendFile(m.chat, bv, 'picturejpg', null, m, true, { type: 'pictureMessage', ptt: true })
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 }
 
-handler.customPrefix = /^( ون بيس عمك|عمك|ميكاسا مين عمك)$/i
+handler.customPrefix = /^(ون بيس عمك)$/i
 handler.command = new RegExp
 
 export default handler
