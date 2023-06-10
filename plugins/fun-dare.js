a2ae659a974c9535e5020fd3a0e55b472e90131e
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-09 18:45:57 -0700

    Update fun-dare.js

--- plugins/fun-dare.js
+++ plugins/fun-dare.js
@@ -5,12 +5,12 @@ let handler  = async (m, { conn }) => {
   if (!res.ok) throw await res.text()
 	    let json = await res.json()
 
-  let guru = `${json.result}`
-  conn.sendMessage(m.chat, { text: guru, mentions: [m.sender] }, { quoted: m })
+  let mikasa = `${json.result}`
+  conn.sendMessage(m.chat, { text: mikasa, mentions: [m.sender] }, { quoted: m })
 }
 handler.help = ['dare']
 handler.tags = ['fun']
-handler.command = /^(dare)$/i
+handler.command = /^(عزيزي)$/i
 
 export default handler
 
