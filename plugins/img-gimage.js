e58c7698997881a842fa1c131bbec612626c1661
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-09 19:00:49 -0700

    Update img-gimage.js

--- plugins/img-gimage.js
+++ plugins/img-gimage.js
@@ -4,20 +4,20 @@ import _gis from 'g-i-s'
 let gis = promisify(_gis)
 
 let handler  = async (m, { conn, args, text, usedPrefix, command }) => {
-  if (!text) throw `✳️ Enter the image you want to search for \n\n📌 Example: *${usedPrefix + command}* Billie Eilish`
+  if (!text) throw `❖ ادخل اسم الصورة الذي تريد البحث عنها\n\n📌 مثال: *${usedPrefix + command}* Gojo Saturo`
   let results = await gis(text) || []
   let { url, width, height } = pickRandom(results) || {}
-  if (!url) throw '❎ image not found try another'
+  if (!url) throw '❎ لم اجد الصوره'
   conn.sendFile(m.chat, url, 'img.png', `
-✅ Result : *${text}*
+✅ النتيجه : *${text}*
 
-⏣ *width*: ${width}
-⏣ *height*: ${height}
+⏣ *عرض*: ${width}
+⏣ *طول*: ${height}
 `.trim(), m)
 }
 handler.help = ['imagen']
 handler.tags = ['img']
-handler.command = /^(img|image|gimage|imagen)$/i
+handler.command = /^(img|image|صوره|ص)$/i
 handler.diamond = true
 
 export default handler
