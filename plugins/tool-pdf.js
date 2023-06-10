0a7878c1a442d90248f15716e120b62551cbd9a5
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-10 08:31:34 -0700

    Update tool-pdf.js

--- plugins/tool-pdf.js
+++ plugins/tool-pdf.js
@@ -2,10 +2,10 @@ import uploadImage from '../lib/uploadImage.js'
 let handler = async (m, { conn, text, usedPrefix, command, isOwner }) => {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
-if (!mime) throw '*Respond / reply to an image*'    
+if (!mime) throw '*رد على رساله*'    
 let img = await q.download?.()
 let url = await uploadImage(img)    
-let docname = text ? text : m.pushName || 'Gurubot'
+let docname = text ? text : m.pushName || 'Mikasabot'
 conn.sendFile(m.chat, `http://api.lolhuman.xyz/api/convert/imgtopdf?apikey=${lolkeysapi}&img=${url}`, docname + '.pdf', '', m, false, { asDocument: true })
 }
 handler.command = /^topdf$/i
