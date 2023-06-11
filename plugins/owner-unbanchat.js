bcc95307e8cfcf5ea788797c09a492884484c1fe
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-11 04:43:44 -0700

    Update owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -3,10 +3,10 @@
 let handler = async (m, { conn, isOwner, isAdmin, isROwner} ) => {
     if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = false
-    m.reply('✅ Bot active in this group')   
+    m.reply('✅ البوت شغال في هذه المجموعه')   
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
-handler.command = ['chaton', 'unbanchat'] 
+handler.command = ['chaton', 'تفعيل'] 
 
 export default handler
