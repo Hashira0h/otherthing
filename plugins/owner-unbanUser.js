1d4a3ad450eaa964d3294bf8e1e0c6cfa82340bc
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-11 04:42:30 -0700

    Update owner-unbanUser.js

--- plugins/owner-unbanUser.js
+++ plugins/owner-unbanUser.js
@@ -5,18 +5,16 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
     let user = global.db.data.users[who]
-    if (!who) throw `✳️ Tag or mention the user to unban`
+    if (!who) throw `❒ منشن الشخص لفك البان`
     let users = global.db.data.users
     users[who].banned = false
     conn.reply(m.chat, `
-✅ UNBAN
-
-───────────
+تم الغاء البان !
 @${who.split`@`[0]} has been unbanned`, m, { mentions: [who] })
 }
 handler.help = ['unban @user']
 handler.tags = ['owner']
-handler.command = /^unban$/i
+handler.command = /^الغاء_البان$/i
 handler.rowner = true
 
 export default handler
