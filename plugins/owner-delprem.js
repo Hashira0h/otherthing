f942d211e7aa50c729aa32ad916a837fafddc919
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-11 03:57:22 -0700

    Update owner-delprem.js

--- plugins/owner-delprem.js
+++ plugins/owner-delprem.js
@@ -5,16 +5,16 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false
     else who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
     let user = global.db.data.users[who]
-    if (!who) return m.reply(`✳️ Mention the user\n\n📌 *Example* :\n${usedPrefix + command} @tag`)
+    if (!who) return m.reply(`⎗ منشن الشخص\n\n⎔ *مثال* :\n${usedPrefix + command} @منشن`)
     if (!global.prems.includes(who.split`@`[0])) throw '✳️ The user is not Premium'
     let index = global.prems.findIndex(v => (v.replace(/[^0-9]/g, '') + '@s.whatsapp.net') === (who.replace(/[^0-9]/g, '') + '@s.whatsapp.net'))
     global.prems.splice(index, 1)
-    conn.reply(m.chat, `✅ Premium removed \n\n@${who.split('@')[0]} you are no longer premium`, m, { mentions: [who] })
+    conn.reply(m.chat, `✅ تم حذف البريميام \n@${who.split('@')[0]} لم يعد لديك بريميام`, m, { mentions: [who] })
     
 }
 handler.help = ['delprem @user']
 handler.tags = ['owner']
-handler.command = ['delprem', 'delpremium'] 
+handler.command = ['delprem', 'حذف_بريميام'] 
 
 handler.group = true
 handler.rowner = true
