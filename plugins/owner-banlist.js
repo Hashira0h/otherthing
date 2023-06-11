a955f9d990343fdf61797a0afc52568ee0155ae1
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-11 03:52:17 -0700

    Update owner-banlist.js

--- plugins/owner-banlist.js
+++ plugins/owner-banlist.js
@@ -5,18 +5,18 @@ let handler = async (m, { conn, usedPrefix }) => {
     let users = Object.entries(global.db.data.users).filter(user => user[1].banned)
     
     m.reply(`
-≡ *USERS BANNED*
+◖!◗ *الاشـخـاص الـمـبـنده* ◖!◗
 
-▢ Total : *${users.length}* 
+□ عددهم : *${users.length}* 
 
 ${users ? '\n' + users.map(([jid], i) => `
-${i + 1}. ${conn.getName(jid) == undefined ? 'UNKNOWN' : conn.getName(jid)}
-▢ ${jid}
+${i + 1}. ${conn.getName(jid) == undefined ? 'غـيـر مـعروف !' : conn.getName(jid)}
+□ ${jid}
 `.trim()).join('\n') : ''}
 `.trim())
 }
 handler.help = ['listban']
 handler.tags = ['owner']
-handler.command = ['banlist', 'listban'] 
+handler.command = ['banlist', 'المبندين'] 
 
 export default handler
