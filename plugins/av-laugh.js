7fd0b0c85d9adac75d08279152c22e0be0079010
Hajimi11 <136582479+Hajimi11@users.noreply.github.com>
2023-06-19 06:20:32 -0700

    Update av-laugh.js

--- plugins/av-laugh.js
+++ plugins/av-laugh.js
@@ -6,7 +6,7 @@ let username = conn.getName(who)
 let bv = `./src/mp3/${pickRandom([""])}.jpg`
 let av = `./src/mp3/${pickRandom([""])}.mp3`
 
-m.reply( `*دوووم الضحكه يابعد عمري 💗*`)
+m.reply( `دووم الضحكه :-D`)
 conn.sendFile(m.chat, bv, 'picturejpg', null, m, true, { type: 'pictureMessage', ptt: true })
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 }
