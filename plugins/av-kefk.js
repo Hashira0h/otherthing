94a3b99d278ddcf89f765f0f766d7521f023f341
Hajimi11 <136582479+Hajimi11@users.noreply.github.com>
2023-06-19 06:19:44 -0700

    Update av-kefk.js

--- plugins/av-kefk.js
+++ plugins/av-kefk.js
@@ -6,7 +6,7 @@ let username = conn.getName(who)
 let bv = `./src/mp3/${pickRandom([""])}.jpg`
 let av = `./src/mp3/${pickRandom([""])}.mp3`
 
-m.reply( `*بخييير الحمدلله، كيفك انت يعمري 💗*`)
+m.reply( `بخير الحمدلله وانت`)
 conn.sendFile(m.chat, bv, 'picturejpg', null, m, true, { type: 'pictureMessage', ptt: true })
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 }
