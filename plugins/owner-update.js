005881c936f99fc1313ea19431f206eb840935c9
Kazatora <137078405+Kazatora@users.noreply.github.com>
2023-06-30 15:20:21 -0700

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -9,7 +9,7 @@ conn.reply(m.chat, stdout.toString(), m)
 }}
 handler.help = ['update']
 handler.tags = ['owner']
-handler.command = ['update', 'actualizar', 'fix', 'fixed'] 
+handler.command = ['update', 'actualizar', 'fix', 'تحديث'] 
 handler.rowner = true
 
 export default handler
