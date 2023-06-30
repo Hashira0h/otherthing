6b8a647fc9ea1ed87aacc83658928516a27e6772
Kazatora <137078405+Kazatora@users.noreply.github.com>
2023-06-30 14:25:11 -0700

    Update tools-calc.js

--- plugins/tools-calc.js
+++ plugins/tools-calc.js
@@ -26,13 +26,13 @@ let handler = async (m, { conn, text }) => {
     if (!result) throw result
     m.reply(`*${format}* = _${result}_`)
   } catch (e) {
-    if (e == undefined) throw '✳️ enter the equation\n\ncompatible symbols -, +, *, /, ×, ÷, π, e, (, )'
-    throw 'Incorrect format, only 0-9 and symbol -, +, *, /, ×, ÷, π, e, (, ) what can you use'
+    if (e == undefined) throw '✳️ ادخل المعاديه\n\nرموز متوافقة -, +, *, /, ×, ÷, π, e, (, )'
+    throw 'الترتيب خاطئ, فقط 0-9 و الرمز -, +, *, /, ×, ÷, π, e, (, ) what can you use'
   }
 }
 handler.help = ['cal <equation>']
 handler.tags = ['tools']
-handler.command = ['cal', 'calc', 'calcular', 'calculadora'] 
+handler.command = ['cal', 'calc', 'calcular', 'احسب'] 
 handler.exp = 5
 
 export default handler
