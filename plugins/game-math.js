2404fff42ed9013641bd4139f529a865ccb3ee31
Kazatora <137078405+Kazatora@users.noreply.github.com>
2023-07-01 06:01:28 -0700

    Update game-math.js

--- plugins/game-math.js
+++ plugins/game-math.js
@@ -2,46 +2,46 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     conn.math = conn.math ? conn.math : {}
     
     if (args.length < 1) throw `
-  🧮 available difficulties : 
+  🧮 المستويات المتوفره : 
   
 ${Object.keys(modes).join(' | ')} 
 
-_📌Example : ${usedPrefix+command} normal_
+*📌مثال : ${usedPrefix+command} متوسط*
 `.trim()
   let mode = args[0].toLowerCase()
   if (!(mode in modes)) throw `
-  🧮 available difficulties : 
+  🧮 المستويات المتوفره : 
   
  ${Object.keys(modes).join(' | ')}
 
-_📌Example : ${usedPrefix+command} normal_
+*📌مثال : ${usedPrefix+command} متوسط*
 `.trim()
     
   let id = m.chat
-    if (id in conn.math) return conn.reply(m.chat, '⚠️ There are still unanswered questions in this chat', conn.math[id][0])
+    if (id in conn.math) return conn.reply(m.chat, '⚠️ لم يجاوب احد السؤال الحالي !', conn.math[id][0])
     let math = genMath(mode)
     conn.math[id] = [
-        await conn.reply(m.chat, `▢ HOW MUCH IS IT *${math.str}*=\n\n_Time:_ ${(math.time / 1000).toFixed(2)} seconds\n\n🎁 Reward : ${math.bonus} XP`, m),
+        await conn.reply(m.chat, `▢ السؤال:/n*${math.str}*=\n\n_الوقت:_ ${(math.time / 1000).toFixed(2)} seconds\n\n🎁 الجائزه : ${math.bonus} XP`, m),
         math, 4,
         setTimeout(() => {
-            if (conn.math[id]) conn.reply(m.chat, `⏳ Time is over!\nThe answer is : *${math.result}*`, conn.math[id][0])
+            if (conn.math[id]) conn.reply(m.chat, `⏳ الوقت انتهى!\nالاجابه : *${math.result}*`, conn.math[id][0])
       delete conn.math[id]
         }, math.time)
     ]
 }
 handler.help = ['Maths <modes>']
 handler.tags = ['game']
-handler.command = ['maths', 'math', 'matemáticas', 'ganit'] 
+handler.command = ['maths', 'math', 'حساب', 'ماث'] 
 
 
 let modes = {
-    noob: [-3, 3,-3, 3, '+-', 15000, 10],
-  easy: [-10, 10, -10, 10, '*/+-', 20000, 40],
-  normal: [-40, 40, -20, 20, '*/+-', 40000, 150],
-  hard: [-100, 100, -70, 70, '*/+-', 60000, 350],
-  extreme: [-999999, 999999, -999999, 999999, '*/', 99999, 9999],
-  impossible: [-99999999999, 99999999999, -99999999999, 999999999999, '*/', 30000, 35000],
-  impossible2: [-999999999999999, 999999999999999, -999, 999, '/', 30000, 50000]
+    نوب: [-3, 3,-3, 3, '+-', 15000, 10],
+  سهل: [-10, 10, -10, 10, '*/+-', 20000, 40],
+  متوسط: [-40, 40, -20, 20, '*/+-', 40000, 150],
+  صعب: [-100, 100, -70, 70, '*/+-', 60000, 350],
+  صعب2: [-999999, 999999, -999999, 999999, '*/', 99999, 9999],
+  مستحيل: [-99999999999, 99999999999, -99999999999, 999999999999, '*/', 30000, 35000],
+  مستحيل2: [-999999999999999, 999999999999999, -999, 999, '/', 30000, 50000]
 }
 
 let operators = {
