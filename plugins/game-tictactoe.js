f189f19a56f79216d69b3577a1f576aeb767358c
Kazatora <137078405+Kazatora@users.noreply.github.com>
2023-07-01 06:05:53 -0700

    Update game-tictactoe.js

--- plugins/game-tictactoe.js
+++ plugins/game-tictactoe.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
     let room = Object.values(conn.game).find(room => room.state === 'WAITING' && (text ? room.name === text : true))
     // m.reply('[WIP Feature]')
     if (room) {
-        m.reply('❏ تم اجاد الشخص الاخر')
+        m.reply('*تم ايجاد الشخص الاخر*')
         room.o = m.chat
         room.game.playerO = m.sender
         room.state = 'PLAYING'
