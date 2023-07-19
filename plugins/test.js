51c8ecae19e595d283a8c26700b367fd0988e748
Hashira0h <139925219+Hashira0h@users.noreply.github.com>
2023-07-19 01:55:32 -0700

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -5,6 +5,9 @@ let chat = global.db.data.chats[m.chat]
     conn.reply(m.chat, `الاستماره`, m) //wm, null, [['Menu', '#menu']], m) botones :V
 
 } 
+     if (/^برب$/m.test(201206178781) ) { //sem prefixo
+    conn.reply(m.chat, `ويلكم باك يا عمنا`, m) //wm, null, [['Menu', '#menu']], m) botones :V
 
+} 
 return !0 }
 export default handler
