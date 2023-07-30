0b486ee24925bb1adbf81e8cac9af643e74b762d
GOJO1! <139925219+Hashira0h@users.noreply.github.com>
2023-07-30 04:45:10 -0700

    Update ارفعني_ادمن.js

--- plugins/ارفعني_ادمن.js
+++ plugins/ارفعني_ادمن.js
@@ -3,7 +3,7 @@ let user = global.db.data.users[m.sender]
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 let username = conn.getName(who)
-let bv = `./sticker/${pickRandom(["st1"])}.jpg`
+let bv = `./sticker/${pickRandom(["st1"])}.webp`
 
 conn.sendFile(m.chat, bv, 'sticker.webp', null, m, true, { type: 'pictureMessage', ptt: true })
 }
