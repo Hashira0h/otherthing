a88c307e76e41fc2ef037751646be25b73aa48be
GOJO1! <139925219+Hashira0h@users.noreply.github.com>
2023-08-07 09:02:32 -0700

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,8 +1,22 @@
-let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems })  => m.reply(`
-لسه ما انضافت
-`.trim())
-handler.help = ['allmenu']
-handler.tags = ['main']
-handler.command = ['ا', 'اوامر'] 
+import { createHash } from 'crypto'
+import PhoneNumber from 'awesome-phonenumber'
+import { canLevelUp, xpRange } from '../lib/levelling.js'
+//import db from '../lib/database.js'
+
+let handler = async (m, { conn, usedPrefix, command}) => {
+
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let user = global.db.data.users[who]
+let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn } = global.db.data.users[who]
+let username = conn.getName(who)
+
+let str = `لسه ما انضافت !`
+    conn.sendFile(m.chat, `https://telegra.ph/file/c15fce842890d1eb01a37.jpg`, 'perfil.jpg', str, m, false, { mentions: [who] })
+    m.react()
+
+}
+handler.help = ['perfil']
+handler.tags = ['group']
+handler.command = ['اوامر'] 
 
 export default handler
