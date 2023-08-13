fc954e04151c6e421d40be3ee66dac8c1f85e84f
GOJO1! <139925219+Hashira0h@users.noreply.github.com>
2023-08-13 14:26:00 +0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -10,7 +10,91 @@ let user = global.db.data.users[who]
 let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn } = global.db.data.users[who]
 let username = conn.getName(who)
 
-let str = `لسه ما انضافت !`
+let str = `*مـرحــباً، مـعـك بـوت غــوجــو*
+
+    ⌯    الـوقـت : 
+    ⌯    الـتاريـخ : 
+
+
+    〢━ 〄 مـعلومات الـبوت 〄 ━〢
+
+
+    ⌯ اسـم الـبوت : غوجو
+    ⌯ حط قبل كل امر :  .
+    ⌯ اســم الـمطور : 𝐌𝐎_𝐘𝐓
+    ⌯ البوت شغـال مـنذ : 
+    ⌯ الـمنـصه : REPLIT
+
+
+〢━ 〄 الـمفـيد 〄 ━〢
+
+
+    ⌯
+    ⌯
+    ⌯
+    ⌯
+    ⌯
+    ⌯
+    ⌯
+  
+
+〢━ ⌬ الـمطـور فـقط ⌬ ━〢
+
+
+    ⌯
+
+
+〢━ ❅ الـجروبـات ❅ ━〢
+
+
+    ⌯
+
+
+〢 ━━ 🔍 الـبحـث 🔎 ━━━〢
+    
+       
+    ⌯     
+ 
+  
+〢━ 📈 الـفلوس 📈 ━〢
+  
+
+    ⌯
+  
+
+〢━ 🎮 الـالـعاب 🎮 ━〢
+
+
+    ⌯
+   
+
+〢━ 🛠️ الـتحـويل 🛠️ ━〢
+   
+    ⌯
+
+
+〢━ ◈ الـصوتـيات ◈ ━〢
+
+
+    ⌯
+ 
+
+〢━ 📥 الـتـنزيـل 📥 ━〢
+   
+  
+    ⌯     
+  
+   
+〢━ 🎐 الـترفــيه 🎐 ━〢
+
+    ⌯
+
+                  *╭───── • ◈ • ─────╮*
+
+*⋄ نـقابـة الـبوت : https://chat.whatsapp.com/BYcNmfA9dbzDddTFrsAdoc*
+*⋄ رقـم الـمطـور :https://wa.me/+201206178781*
+
+                  *╰───── • ◈ • ─────╯* `
     conn.sendFile(m.chat, `https://telegra.ph/file/c15fce842890d1eb01a37.jpg`, 'perfil.jpg', str, m, false, { mentions: [who] })
     m.react()
 
