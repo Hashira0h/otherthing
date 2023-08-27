763dff67ba9f7dec3d344de6381affcde58825ea
GOJO1! <139925219+Hashira0h@users.noreply.github.com>
2023-08-26 23:49:13 -0700

    Create _autoreply.js

---
+++ plugins/_autoreply.js
@@ -0,0 +1,15 @@
+export async function all(m) {
+	
+  // when someone sends a group link to the bot's dm
+  if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('open this link')) && !m.isBaileys && !m.isGroup) {
+   this.sendMessage(m.chat,{text:`تقدر تكلم المطور اذا اردت البوت الدخول الى مجموعتك\n\n*اكتب* *.المطور* *للتواصل مع المطور*`.trim()}, {quoted:m});
+   /*this.sendButton(m.chat, `*Invite bot to a group*      
+    Hallo @${m.sender.split('@')[0]} 
+    you can rent the bot to join a group or contact owner 
+    more info click on the button
+  `.trim(), igfg, null, [['Rent', '/buyprem']] , m, { mentions: [m.sender] })*/
+   m.react('💎')
+} 
+
+ return !0
+}
