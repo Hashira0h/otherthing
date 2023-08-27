44c0066d3bdf79020fcbca0abfe7366da76ad4d6
GOJO1! <139925219+Hashira0h@users.noreply.github.com>
2023-08-27 11:08:18 -0700

    Update dl-ytv.js

--- plugins/dl-ytv.js
+++ plugins/dl-ytv.js
@@ -4,46 +4,70 @@ import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
 let limit = 350
 let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
 	if (!args || !args[0]) throw `❖ مثال :\n${usedPrefix + command} https://youtu.be/pO5dgZrM9Mk`
-    if (!args[0].match(/youtu/gi)) throw `❎ تاكد من ان الرابط من يوتيوب`
-	 let chat = global.db.data.chats[m.chat]
-	 m.react(rwait) 
-	try {
-		let q = args[1] || '360p'
-		let v = args[0]
-		const yt = await youtubedl(v).catch(async () => await youtubedlv2(v)).catch(async () => await youtubedlv3(v))
-		const dl_url = await yt.video[q].download()
-		const title = await yt.title
-		const size = await yt.video[q].fileSizeH 
-		
-       if (size.split('MB')[0] >= limit) return m.reply(` 〖 تحميل من يوتيوب 〗◎ *⚖️المساحه* : ${size}\n◎ *🎞️الجوده* : ${q}\n\n◎ _لا يمكنني تحميله_ *+${limit} MB*`)    
-	  conn.sendFile(m.chat, dl_url, title + '.mp4', `
- ☃  *Mikasa YTDL*
-  
-◎ *📌العنوان* : ${title}
-◎ *📟 صنفه* : mp4
-◎ *🎞️الجوده* : ${q}
-◎ *⚖️المساحه* : ${size}
-`.trim(), m, false, { asDocument: chat.useDocument })
-		m.react(done) 
-		
-	} catch {
-		
-		const { title, result, quality, size, duration, thumb, channel } = await fg.ytv(args[0]) 
-		if (size.split('MB')[0] >= limit) return m.reply(` 〖 تحميل من يوتيوب 〗\n\n❖ *⚖️المساحه* : ${size}\n❖ *🎞️الجوده* : ${quality}\n\n❖ _تجاوز الملف حد التنزيل_ *+${limit} MB*`)
-	conn.sendFile(m.chat, result, title + '.mp4', `
-〖 تحميل من يوتيوب 〗
-  
-❖ *📌 العنوان* : ${title}
-❖ *📟 نوع الملف* : mp4
-❖ *⚖️ المساحه* : ${size}
-`.trim(), m, false, { asDocument: chat.useDocument })
-		m.react(done) 
-	} 
-		 
-}
-handler.help = ['ytmp4 <link yt>']
-handler.tags = ['dl'] 
-handler.command = ['ytmp4', 'فيديو']
-handler.diamond = true
+    if (!args[0].match(/youtu/gi)) throw `❎ تاكد من ان الرابط من يوتيوب`import ytdl from 'ytdl-core';
+import fs from 'fs';
+import os from 'os';
 
-export default handler
+let limit = 500;
+let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
+  if (!args || !args[0]) throw `*✳️ مــثال:\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI*`;
+  if (!args[0].match(/youtu/gi)) throw `*❎ تـأكـد أنـه رابـط مـن يـوتيـوب*`;
+
+  let chat = global.db.data.chats[m.chat];
+  m.react(rwait);
+  try {
+    const info = await ytdl.getInfo(args[0]);
+    const format = ytdl.chooseFormat(info.formats, { quality: 'highest' });
+    if (!format) {
+      throw new Error('No valid formats found');
+    }
+
+    if (format.contentLength / (1024 * 1024) >= limit) {
+      return m.reply(`❒ *⚖️ الـمسـاحـه*: ${format.contentLength / (1024 * 1024).toFixed(2)}MB\n❒ *🎞️ الـجـوده*: ${format.qualityLabel}\n\n*❒ الـملـف تـجاوز حـد الـمساحــه* *+${limit} MB*`);
+    }
+
+    const tmpDir = os.tmpdir();
+    const fileName = `${tmpDir}/${info.videoDetails.videoId}.mp4`;
+
+    const writableStream = fs.createWriteStream(fileName);
+    ytdl(args[0], {
+      quality: format.itag,
+    }).pipe(writableStream);
+
+    writableStream.on('finish', () => {
+      conn.sendFile(
+        m.chat,
+        fs.readFileSync(fileName),
+        `${info.videoDetails.videoId}.mp4`,
+        `*❖───┊ ♪ يــوتـــيــوب ♪ ┊───❖*
+	  
+	  *❏ الـعـنوان: ${info.videoDetails.title}*
+	  *❐ الـمده: ${info.videoDetails.lengthSeconds} seconds*
+	  *❑ الـمـشاهدات: ${info.videoDetails.viewCount}*
+	  *❒ وقــت الـرفـع: ${info.videoDetails.publishDate}*
+	  *❒ الـرابـط: ${args[0]}*`,
+        m,
+        false,
+        { asDocument: chat.useDocument }
+      );
+
+      fs.unlinkSync(fileName); // Delete the temporary file
+      m.react(done);
+    });
+
+    writableStream.on('error', (error) => {
+      console.error(error);
+      m.reply('*جـرب لاحـقاً !*');
+    });
+  } catch (error) {
+    console.error(error);
+    m.reply('*جـرب لاحـقاً !*');
+  }
+};
+
+handler.help = ['ytmp4 <yt-link>'];
+handler.tags = ['dl'];
+handler.command = ['فيديو', 'video'];
+handler.diamond = false;
+
+export default handler;
