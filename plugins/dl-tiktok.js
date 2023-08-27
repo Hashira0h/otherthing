8a9ca688002fe89398a653e2faa3c242fb0059ea
GOJO1! <139925219+Hashira0h@users.noreply.github.com>
2023-08-27 11:58:13 -0700

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -1,38 +1,31 @@
+import { tiktokdl } from '@bochilteam/scraper';
 
-import fg from 'api-dylux' 
-import { tiktokdl, tiktokdlv2, tiktokdlv3 } from '@bochilteam/scraper'
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+  if (!args[0]) throw `*✳️ أدخـل رابــط تـيكتـوك\n\n📌 مــثال: ${usedPrefix + command} https://vm.tiktok.com/ZMYG92bUh/`;
+  if (!args[0].match(/tiktok/gi)) throw `*❎ رابط تيكتوك !!*`;
 
-let handler = async (m, { conn, text, args, usedPrefix, command}) => {
-if (!args[0]) throw `☆ ادخل رابط تيك توك\n\n 📌 مثال : ${usedPrefix + command} https://www.tiktok.com/@svsvkes/video/7177431175273794822?is_from_webapp=1&sender_device=pc&web_id=7242157033230681606`
-if (!args[0].match(/tiktok/gi)) throw `❎ تأكد ان هذا الرابط من تيك توك`
-m.react(rwait)
+  try {
+    const { author: { nickname }, video, description } = await tiktokdl(args[0]);
+    const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw;
+    if (!url) throw '❎ حـدث خـطأ ما اثـناء تـنزيل الـفيديــو';
 
-try {
-    let p = await fg.tiktok(args[0]) 
-    let te = `
-■━━【 تيك توك 】━━■
-□ *اسم المستخدم:* ${p.author}
-□ *الوصف:* ${p.title}
-└───────────────`
-    conn.sendButton(m.chat, te, igfg, p.nowm, [['⎘ Stalkig', `${usedPrefix}ttstalk ${p.author.replace(/^@/, '')}`], ['♫ Audio', `${usedPrefix}tomp3`]], m)
-    m.react(done)
-    } catch {  	
-	const { author: { nickname }, video, description } = await tiktokdl(args[0])
-         .catch(async _ => await tiktokdlv2(args[0]))
-         .catch(async _ => await tiktokdlv3(args[0]))
-    const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
-    if (!url) throw '❎ حدث خطأ ما اثناء تنزيل الفيديو'
-    conn.sendButton(m.chat, `
-■━━【 تيك توك 】━━■
-□ *الاسم:* ${nickname} ${description ? `\n▢ *Description:* ${description}` : ''}
-└───────────────`, igfg, url, [['♫ Audio', `${usedPrefix}tomp3`]], m)
-m.react(done)
-} 
-    
-}  
-handler.help = ['tiktok']
-handler.tags = ['dl']
-handler.command = /^(tiktok|ttdl|tiktokdl|تيكتوك)$/i
-handler.diamond = true
+   
 
-export default handler
+    conn.sendFile(m.chat, url, 'tiktok.mp4', `
+*❖━━━┋ تــيك تــوك ┋━━━❖*
+❒ *الأســم:* ${nickname} ${description ? `\n❒ *الــوصف:* ${description}` : ''}
+`, m);
+  } catch (error) {
+    // Log the error
+    console.error('حـدث خـطأ ما اثـناء تـنزيل الـفيديــو:', args[0], error);
+
+    m.reply(`❎ حـدث خـطأ ما اثـناء تـنزيل الـفيديــو`);
+  }
+};
+
+handler.help = ['tiktok'];
+handler.tags = ['dl'];
+handler.command = /^(tiktok|ttdl|تيك|تيكتوك)$/i;
+
+
+export default handler;
