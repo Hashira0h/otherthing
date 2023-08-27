20708bf6c098f0004d691d893dc7492ac317aa92
GOJO1! <139925219+Hashira0h@users.noreply.github.com>
2023-08-27 12:16:48 -0700

    Update gp-promote.js

--- plugins/gp-promote.js
+++ plugins/gp-promote.js
@@ -19,7 +19,7 @@ var user = number + '@s.whatsapp.net'
 } catch (e) {
 } finally {
 conn.groupParticipantsUpdate(m.chat, [user], 'promote')
-m.reply(`✅ User promoted`)
+m.reply(`*تـمـت الـتـرقـيـه !*`)
 }}
 handler.help = ['promote']
 handler.tags = ['group']
