59259d387681468246a8c37aa7b8c5949c769dd0
GOJO1! <139925219+Hashira0h@users.noreply.github.com>
2023-08-27 11:56:03 -0700

    Update dl-mediafire.js

--- plugins/dl-mediafire.js
+++ plugins/dl-mediafire.js
@@ -6,22 +6,21 @@ let handler = async (m, { conn, args, usedPrefix, command, isOwner, isPrems }) =
 	var limit
      if((isOwner || isPrems)) limit = 1200
      else limit = 100
-   if (!args[0]) throw `*ادخل رابط من ميديافاير بجانب الامر !*`
-    if (!args[0].match(/mediafire/gi)) throw `❎ الرابط غير صحيح`
+   if (!args[0]) throw `*✳️ أدخـل الـرابــط بـجانـب الأمــر*`
+    if (!args[0].match(/mediafire/gi)) throw `*❎ الـرابــط خـاطئ !*`
     m.react(rwait)
     let full = /f$/i.test(command)
     let u = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
-    let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url: u }))).buffer()
+    let ss = await (await fetch(`https://image.thum.io/get/fullpage/${u}`)).buffer()
     let res = await mediafiredl(args[0])
     let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
     let isLimit = (isPrems || isOwner ? limit : limit) * 1012 < filesize
     let caption = `
-   ┓━━━ *ميديا فاير* 
-┫■ *الرقم:* ${filename}
-┫■ *الحجم:* ${filesizeH}
-┫■ *الامتداد:* ${ext}
-┫■ *وقت الرفع:* ${aploud}
-${isLimit ? `\n■ تجاوز الملف حد التنزيل *+${limit} MB*\nقم بالترقية إلى بريميام لتتمكن من تنزيل الملفات أكثر من *900 MB*` : ''} 
+↰ *الــأسـم:* ${filename}
+↰ *الـمســاحه:* ${filesizeH}
+↰ *الـأمـتداد:* ${ext}
+↰ *وقـت الـرفع:* ${aploud}
+${isLimit ? `\n*❖ يـتجاوز الـملـف حـد الـمساحــه المـتاحـه لـك* *+${limit} MB*\n*كـن عـضو بـريـمـيـام لـتسـتطـيع تـنزيل مـلف بـمساحـة* *900 MB*` : ''} 
 `.trim()
     await conn.sendFile(m.chat, ss, 'ssweb.png', caption, m)
     
