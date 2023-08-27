43090f0986318f72b70aba0aceb29eed84215528
GOJO1! <139925219+Hashira0h@users.noreply.github.com>
2023-08-27 11:56:37 -0700

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -1,72 +1,87 @@
 
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
-import yts from 'yt-search'
+import ytdl from 'ytdl-core';
+import yts from 'yt-search';
+import fs from 'fs';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+import os from 'os';
+
+const streamPipeline = promisify(pipeline);
+
 var handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw `مثال: ${usedPrefix}${command} سورة الفاتحه`
-  await m.reply(wait)
-  let search = await yts(text)
-  let vid = search.videos[Math.floor(Math.random() * search.videos.length)]
-  if (!search) throw 'لم اجد الفيديو، جرب عنوان اخر !'
-  let { title, thumbnail, timestamp, views, ago, url } = vid
-  let wm = 'Downloading audio please wait'
+  if (!text) throw `Use example ${usedPrefix}${command} naruto blue bird`;
+  await m.react(rwait);
 
-  let captvid = `╮──── 〔 يوتيوب 〕 ─⬣
-⬡ العنوان: ${title}
-⬡ المدة: ${timestamp}
-⬡ المشاهدات: ${views}
-⬡ موعد النشر: ${ago}
-⬡ الرابط: ${url}
-╰────────⬣`
-  conn.sendButton(m.chat, `╮──── 〔 يوتيوب 〕 ─⬣
-⬡ العنوان: ${title}
-⬡ المدة: ${timestamp}
-⬡ المشاهدات: ${views}
-⬡ موعد النشر: ${ago}
-⬡ الرابط: ${url}
-╰────────⬣`, author.trim(), await( await conn.getFile(thumbnail)).data, ['VIDEO', `${usedPrefix}ytmp4 ${url}`], false, { quoted: m, 'document': { 'url':'https://wa.me/201206178781' },
-'mimetype': global.dpdf,
-'fileName': `مشغل ميكاسا`,
-'fileLength': 666666666666666,
-'pageCount': 666,contextInfo: { externalAdReply: { showAdAttribution: true,
-mediaType:  2,
-mediaUrl: `${url}`,
-title: `جاري الارسال`,
-body: wm,
-sourceUrl: 'http://wa.me/201206178781', thumbnail: await ( await conn.getFile(thumbnail)).data
-  }
- } 
-})
+  let search = await yts(text);
+  let vid = search.videos[Math.floor(Math.random() * search.videos.length)];
+  if (!search) throw 'Video Not Found, Try Another Title';
+  let { title, thumbnail, timestamp, views, ago, url } = vid;
+  let wm = 'Downloading audio please wait';
+
+  let captvid = `*❖───┊ ♪ يــوتـــيــوب ♪ ┊───❖*
+  ❏ الـعـنوان: ${title}
   
-  //let buttons = [{ buttonText: { displayText: '📽VIDEO' }, buttonId: `${usedPrefix}ytv ${url} 360` }]
- //let msg = await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: captvid, footer: author, buttons }, { quoted: m })
+  ❐ الـمده: ${timestamp}
+  
+  ❑ الــمـشهـدات: ${views}
+  
+  ❒ مـنذ: ${ago}
+  
+  ❒ الـرابــط: ${url}`;
 
-  const yt = await youtubedlv2(url).catch(async _ => await youtubedl(url))
-const link = await yt.audio['128kbps'].download()
-  let doc = { 
-  audio: 
-  { 
-    url: link 
-}, 
-mimetype: 'audio/mp4', fileName: `${title}`, contextInfo: { externalAdReply: { showAdAttribution: true,
-mediaType:  2,
-mediaUrl: url,
-title: title,
-body: wm,
-sourceUrl: url,
-thumbnail: await(await conn.getFile(thumbnail)).data                                                                     
-                                                                                                                 }
-                       }
-  }
+  conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: captvid, footer: author }, { quoted: m });
 
-  return conn.sendMessage(m.chat, doc, { quoted: m })
-	// return conn.sendMessage(m.chat, { document: { url: link }, mimetype: 'audio/mpeg', fileName: `${title}.mp3`}, { quoted: m})
-	// return await conn.sendFile(m.chat, link, title + '.mp3', '', m, false, { asDocument: true })
-}
-handler.help = ['play'].map(v => v + ' <query>')
-handler.tags = ['downloader']
-handler.command = /^شغل$/i
 
-handler.exp = 0
-handler.diamond = false
+  const audioStream = ytdl(url, {
+    filter: 'audioonly',
+    quality: 'highestaudio',
+  });
 
-export default handler
+  // Get the path to the system's temporary directory
+  const tmpDir = os.tmpdir();
+
+  // Create writable stream in the temporary directory
+  const writableStream = fs.createWriteStream(`${tmpDir}/${title}.mp3`);
+
+  // Start the download
+  await streamPipeline(audioStream, writableStream);
+
+  let doc = {
+    audio: {
+      url: `${tmpDir}/${title}.mp3`
+    },
+    mimetype: 'audio/mp4',
+    fileName: `${title}`,
+    contextInfo: {
+      externalAdReply: {
+        showAdAttribution: true,
+        mediaType: 2,
+        mediaUrl: url,
+        title: title,
+        body: wm,
+        sourceUrl: url,
+        thumbnail: await (await conn.getFile(thumbnail)).data
+      }
+    }
+  };
+
+  await conn.sendMessage(m.chat, doc, { quoted: m });
+
+  // Delete the audio file
+  fs.unlink(`${tmpDir}/${title}.mp3`, (err) => {
+    if (err) {
+      console.error(`Failed to delete audio file: ${err}`);
+    } else {
+      console.log(`Deleted audio file: ${tmpDir}/${title}.mp3`);
+    }
+  });
+};
+
+handler.help = ['play'].map((v) => v + ' <query>');
+handler.tags = ['downloader'];
+handler.command = /^شغل$/i;
+
+handler.exp = 0;
+handler.diamond = false;
+
+export default handler;
