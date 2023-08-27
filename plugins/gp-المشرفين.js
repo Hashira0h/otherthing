b0c12e605c7ee2ea157b5e93968879c49002f89e
GOJO1! <139925219+Hashira0h@users.noreply.github.com>
2023-08-27 12:07:53 -0700

    Update gp-المشرفين.js

--- plugins/gp-المشرفين.js
+++ plugins/gp-المشرفين.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, participants, groupMetadata, args }) => {
 const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/avatar_contact.png'
 const groupAdmins = participants.filter(p => p.admin)
-const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n♕ ')
+const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 
-let text = `
-❒ *مشرفين المجموعه* _${groupMetadata.subject}_
+let text = `〖 *أريــد مـشـرفاً !* 〗
+❒ جـروب: *${groupMetadata.subject}*
 
-  ${listAdmin}
+${listAdmin}
 `.trim()
 conn.sendFile(m.chat, pp, 'staff.png', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })
 }
