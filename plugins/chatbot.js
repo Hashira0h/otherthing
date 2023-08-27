b53d03f5a3857b24083d0e43cbac627e1180ea5a
GOJO1! <139925219+Hashira0h@users.noreply.github.com>
2023-08-27 12:00:00 -0700

    Delete chatbot.js

--- plugins/chatbot.js
+++
@@ -1,9 +0,0 @@
-let handler = m => m
-handler.all = async function (m) {
-let chat = global.db.data.chats[m.chat] 
-     if (/^هلا$/i.test(m.text) ) { //sem prefixo
-    conn.reply(m.chat, `اهلين منور`, m) //wm, null, [['Menu', '#menu']], m) botones :V
-
-} 
-return !0 }
-export default handler
